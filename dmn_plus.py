44856f8a3ae3fe983c6dc460d779d0ca9e0e4547
smartyining <yg1305@nyu.edu>
2016-12-01 11:43:47 -0500

    init

---
+++ dmn_plus.py
@@ -0,0 +1,398 @@
+import sys
+import time
+
+import numpy as np
+from copy import deepcopy
+
+import tensorflow as tf
+
+import snli_input
+
+class Config(object):
+    """Holds model hyperparams and data information."""
+
+    batch_size = 100
+    embed_size = 80
+    hidden_size = 80
+
+    max_epochs = 256
+    early_stopping = 20
+
+    dropout = 0.9
+    lr = 0.001
+    l2 = 0.001
+
+    cap_grads = False
+    max_grad_val = 10
+    noisy_grads = False
+
+    word2vec_init = False
+    embedding_init = 1.7320508 # root 3
+
+    # set to zero with strong supervision to only train gates
+    strong_supervision = False
+    beta = 1
+
+    drop_grus = False
+
+    anneal_threshold = 1000
+    anneal_by = 1.5
+
+    num_hops = 3
+    num_attention_features = 4
+
+    max_allowed_inputs = 200
+
+    floatX = np.float32
+
+    train_mode = True
+
+def _add_gradient_noise(t, stddev=1e-3, name=None):
+    """Adds gradient noise as described in http://arxiv.org/abs/1511.06807
+    The input Tensor `t` should be a gradient.
+    The output will be `t` + gaussian noise.
+    0.001 was said to be a good fixed value for memory networks."""
+    with tf.op_scope([t, stddev], name, "add_gradient_noise") as name:
+        t = tf.convert_to_tensor(t, name="t")
+        gn = tf.random_normal(tf.shape(t), stddev=stddev)
+        return tf.add(t, gn, name=name)
+
+# from https://github.com/domluna/memn2n
+def _position_encoding(sentence_size, embedding_size):
+    """Position encoding described in section 4.1 in "End to End Memory Networks" (http://arxiv.org/pdf/1503.08895v5.pdf)"""
+    encoding = np.ones((embedding_size, sentence_size), dtype=np.float32)
+    ls = sentence_size+1
+    le = embedding_size+1
+    for i in range(1, le):
+        for j in range(1, ls):
+            encoding[i-1, j-1] = (i - (le-1)/2) * (j - (ls-1)/2)
+    encoding = 1 + 4 * encoding / embedding_size / sentence_size
+    return np.transpose(encoding)
+
+    # TODO fix positional encoding so that it varies according to sentence lengths
+
+def _xavier_weight_init():
+    """Xavier initializer for all variables except embeddings as desribed in [1]"""
+    def _xavier_initializer(shape, **kwargs):
+        eps = np.sqrt(6) / np.sqrt(np.sum(shape))
+        out = tf.random_uniform(shape, minval=-eps, maxval=eps)
+        return out
+    return _xavier_initializer
+
+# from https://danijar.com/variable-sequence-lengths-in-tensorflow/
+# used only for custom attention GRU as TF handles this with the sequence length param for normal RNNs
+def _last_relevant(output, length):
+    """Finds the output at the end of each input"""
+    batch_size = int(output.get_shape()[0])
+    max_length = int(output.get_shape()[1])
+    out_size = int(output.get_shape()[2])
+    index = tf.range(0, batch_size) * max_length + (length - 1)
+    flat = tf.reshape(output, [-1, out_size])
+    relevant = tf.gather(flat, index)
+    return relevant
+    
+
+class DMN_PLUS(object):
+
+    def load_data(self, debug=False):
+        """Loads train/valid/test data and sentence encoding"""
+        if self.config.train_mode:
+            self.train, self.valid, self.word_embedding, self.max_q_len, self.max_input_len,self.vocab_size = snli_input.load_snli(self.config, split_sentences=False)
+        else:
+            self.test, self.word_embedding, self.max_q_len, self.max_input_len, self.vocab_size = snli_input.load_snli(self.config, split_sentences=False)
+        #self.encoding = _position_encoding(self.max_sen_len, self.config.embed_size)
+
+    def add_placeholders(self):
+        """add data placeholder to graph"""
+        self.question_placeholder = tf.placeholder(tf.int32, shape=(self.config.batch_size, self.max_q_len))
+        self.input_placeholder = tf.placeholder(tf.int32, shape=(self.config.batch_size, self.max_input_len))
+
+        self.question_len_placeholder = tf.placeholder(tf.int32, shape=(self.config.batch_size,))
+        self.input_len_placeholder = tf.placeholder(tf.int32, shape=(self.config.batch_size,))
+
+        self.answer_placeholder = tf.placeholder(tf.int64, shape=(self.config.batch_size,))
+
+        self.dropout_placeholder = tf.placeholder(tf.float32)
+
+    def add_reused_variables(self):
+        """Adds trainable variables which are later reused""" 
+        gru_cell = tf.nn.rnn_cell.GRUCell(self.config.hidden_size)
+
+        # apply droput to grus if flag set
+        if self.config.drop_grus:
+            self.gru_cell = tf.nn.rnn_cell.DropoutWrapper(gru_cell, input_keep_prob=self.dropout_placeholder, output_keep_prob=self.dropout_placeholder)
+        else:
+            self.gru_cell = gru_cell
+
+        with tf.variable_scope("memory/attention", initializer=_xavier_weight_init()):
+            b_1 = tf.get_variable("bias_1", (self.config.embed_size,))
+            W_1 = tf.get_variable("W_1", (self.config.embed_size*self.config.num_attention_features, self.config.embed_size))
+
+            W_2 = tf.get_variable("W_2", (self.config.embed_size, 1))
+            b_2 = tf.get_variable("bias_2", 1)
+
+        with tf.variable_scope("memory/attention_gru", initializer=_xavier_weight_init()):
+            Wr = tf.get_variable("Wr", (self.config.embed_size, self.config.hidden_size))
+            Ur = tf.get_variable("Ur", (self.config.hidden_size, self.config.hidden_size))
+            br = tf.get_variable("bias_r", (1, self.config.hidden_size))
+
+            W = tf.get_variable("W", (self.config.embed_size, self.config.hidden_size))
+            U = tf.get_variable("U", (self.config.hidden_size, self.config.hidden_size))
+            bh = tf.get_variable("bias_h", (1, self.config.hidden_size))
+
+    def get_predictions(self, output):
+        """Get answer predictions from output"""
+        preds = tf.nn.softmax(output)
+        pred = tf.argmax(preds, 1)
+        return pred
+      
+    def add_loss_op(self, output):
+        """Calculate loss"""
+        # optional strong supervision of attention with supporting facts
+        gate_loss = 0
+        # if self.config.strong_supervision:
+        #     for i, att in enumerate(self.attentions):
+        #         labels = tf.gather(tf.transpose(self.rel_label_placeholder), 0)
+        #         gate_loss += tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(att, labels))
+
+        loss = self.config.beta*tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(output, self.answer_placeholder)) + gate_loss
+
+        # add l2 regularization for all variables except biases
+        for v in tf.trainable_variables():
+            if not 'bias' in v.name.lower():
+                loss += self.config.l2*tf.nn.l2_loss(v)
+
+        tf.scalar_summary('loss', loss)
+
+        return loss
+        
+    def add_training_op(self, loss):
+        """Calculate and apply gradients"""
+        opt = tf.train.AdamOptimizer(learning_rate=self.config.lr)
+        gvs = opt.compute_gradients(loss)
+
+        # optionally cap and noise gradients to regularize
+        if self.config.cap_grads:
+            gvs = [(tf.clip_by_norm(grad, self.config.max_grad_val), var) for grad, var in gvs]
+        if self.config.noisy_grads:
+            gvs = [(_add_gradient_noise(grad), var) for grad, var in gvs]
+
+        train_op = opt.apply_gradients(gvs)
+        return train_op
+  
+
+    def get_question_representation(self, embeddings):
+        """Get question vectors via embedding and GRU"""
+        questions = tf.nn.embedding_lookup(embeddings, self.question_placeholder)
+
+        questions = tf.split(1, self.max_q_len, questions)
+        questions = [tf.squeeze(q, squeeze_dims=[1]) for q in questions]
+
+        _, q_vec = tf.nn.rnn(self.gru_cell, questions, dtype=np.float32, sequence_length=self.question_len_placeholder)
+        
+        return q_vec
+
+    def get_input_representation(self, embeddings):
+        """Get fact (sentence) vectors via embedding, positional encoding and bi-directional GRU"""
+        # get word vectors from embedding
+        inputs = tf.nn.embedding_lookup(embeddings, self.input_placeholder)
+        # use encoding to get sentence representation
+        # inputs = tf.reduce_sum(inputs * self.encoding, 2)
+
+        inputs = tf.split(1, self.max_input_len, inputs)
+        inputs = [tf.squeeze(i, squeeze_dims=[1]) for i in inputs]
+
+        outputs, _, _ = tf.nn.bidirectional_rnn(self.gru_cell, self.gru_cell, inputs, dtype=np.float32, sequence_length=self.input_len_placeholder)
+
+        # f<-> = f-> + f<-
+        fact_vecs = [tf.reduce_sum(tf.pack(tf.split(1, 2, out)), 0) for out in outputs]
+
+        # apply dropout
+        fact_vecs = [tf.nn.dropout(fv, self.dropout_placeholder) for fv in fact_vecs]
+
+        return fact_vecs
+
+    def get_attention(self, q_vec, prev_memory, fact_vec):
+        """Use question vector and previous memory to create scalar attention for current fact"""
+        with tf.variable_scope("attention", reuse=True, initializer=_xavier_weight_init()):
+
+            W_1 = tf.get_variable("W_1")
+            b_1 = tf.get_variable("bias_1")
+
+            W_2 = tf.get_variable("W_2")
+            b_2 = tf.get_variable("bias_2")
+
+            features = [fact_vec*q_vec, fact_vec*prev_memory, tf.abs(fact_vec - q_vec), tf.abs(fact_vec - prev_memory)]
+
+            feature_vec = tf.concat(1, features)
+
+            attention = tf.matmul(tf.tanh(tf.matmul(feature_vec, W_1) + b_1), W_2) + b_2
+            
+        return attention
+
+    def _attention_GRU_step(self, rnn_input, h, g):
+        """Implement attention GRU as described by https://arxiv.org/abs/1603.01417"""
+        with tf.variable_scope("attention_gru", reuse=True, initializer=_xavier_weight_init()):
+
+            Wr = tf.get_variable("Wr")
+            Ur = tf.get_variable("Ur")
+            br = tf.get_variable("bias_r")
+
+            W = tf.get_variable("W")
+            U = tf.get_variable("U")
+            bh = tf.get_variable("bias_h")
+
+            r = tf.sigmoid(tf.matmul(rnn_input, Wr) + tf.matmul(h, Ur) + br)
+            h_hat = tf.tanh(tf.matmul(rnn_input, W) + r*tf.matmul(h, U) + bh)
+            rnn_output = g*h_hat + (1-g)*h
+
+            return rnn_output
+
+    def generate_episode(self, memory, q_vec, fact_vecs):
+        """Generate episode by applying attention to current fact vectors through a modified GRU"""
+
+        attentions = [tf.squeeze(self.get_attention(q_vec, memory, fv), squeeze_dims=[1]) for fv in fact_vecs]
+
+        attentions = tf.transpose(tf.pack(attentions))
+
+        self.attentions.append(attentions)
+
+        softs = tf.nn.softmax(attentions)
+        softs = tf.split(1, self.max_input_len, softs)
+        
+        gru_outputs = []
+
+        # set initial state to zero
+        h = tf.zeros((self.config.batch_size, self.config.hidden_size))
+
+        # use attention gru
+        for i, fv in enumerate(fact_vecs):
+            h = self._attention_GRU_step(fv, h, softs[i])
+            gru_outputs.append(h)
+
+        # extract gru outputs at proper index according to input_lens
+        gru_outputs = tf.pack(gru_outputs)
+        gru_outputs = tf.transpose(gru_outputs, perm=[1,0,2])
+        episode = _last_relevant(gru_outputs, self.input_len_placeholder)
+
+        return episode
+
+    def add_answer_module(self, rnn_output, q_vec):
+        """Linear softmax answer module"""
+        with tf.variable_scope("answer"):
+
+            rnn_output = tf.nn.dropout(rnn_output, self.dropout_placeholder)
+
+            U = tf.get_variable("U", (2*self.config.embed_size, 3))
+            b_p = tf.get_variable("bias_p", (3,))
+
+            output = tf.matmul(tf.concat(1, [rnn_output, q_vec]), U) + b_p
+
+
+            return output
+
+    def inference(self):
+        """Performs inference on the DMN model"""
+
+        # set up embedding
+        embeddings = tf.Variable(self.word_embedding.astype(np.float32), name="Embedding")
+         
+        # input fusion module
+        with tf.variable_scope("question", initializer=_xavier_weight_init()):
+            print '==> get question representation'
+            q_vec = self.get_question_representation(embeddings)
+         
+
+        with tf.variable_scope("input", initializer=_xavier_weight_init()):
+            print '==> get input representation'
+            fact_vecs = self.get_input_representation(embeddings)
+
+        # keep track of attentions for possible strong supervision
+        self.attentions = []
+
+        # memory module
+        with tf.variable_scope("memory", initializer=_xavier_weight_init()):
+            print '==> build episodic memory'
+
+            # generate n_hops episodes
+            prev_memory = q_vec
+
+            for i in range(self.config.num_hops):
+                # get a new episode
+                print '==> generating episode', i
+                episode = self.generate_episode(prev_memory, q_vec, fact_vecs)
+
+                # untied weights for memory update
+                Wt = tf.get_variable("W_t"+ str(i), (2*self.config.hidden_size+self.config.embed_size, self.config.hidden_size))
+                bt = tf.get_variable("bias_t"+ str(i), (self.config.hidden_size,))
+
+                # update memory with Relu
+                prev_memory = tf.nn.relu(tf.matmul(tf.concat(1, [prev_memory, episode, q_vec]), Wt) + bt)
+
+            output = prev_memory
+
+        # pass memory module output through linear answer module
+        output = self.add_answer_module(output, q_vec)
+
+        return output
+
+
+    def run_epoch(self, session, data, num_epoch=0, train_writer=None, train_op=None, verbose=2, train=False):
+        config = self.config
+        dp = config.dropout
+        if train_op is None:
+            train_op = tf.no_op()
+            dp = 1
+        total_steps = len(data[0]) / config.batch_size
+        total_loss = []
+        accuracy = 0
+        
+        # shuffle data
+        p = np.random.permutation(len(data[0]))
+        qp, ip, ql, il, im, a, r = data
+        qp, ip, ql, il, im, a, r = qp[p], ip[p], ql[p], il[p], im[p], a[p], r[p] 
+
+        for step in range(total_steps):
+            index = range(step*config.batch_size,(step+1)*config.batch_size)
+            feed = {self.question_placeholder: qp[index],
+                  self.input_placeholder: ip[index],
+                  self.question_len_placeholder: ql[index],
+                  self.input_len_placeholder: il[index],
+                  self.answer_placeholder: a[index],
+                  self.dropout_placeholder: dp}
+            loss, pred, summary, _ = session.run(
+              [self.calculate_loss, self.pred, self.merged, train_op], feed_dict=feed)
+
+            if train_writer is not None:
+                train_writer.add_summary(summary, num_epoch*total_steps + step)
+
+            answers = a[step*config.batch_size:(step+1)*config.batch_size]
+            accuracy += np.sum(pred == answers)/float(len(answers))
+
+
+            total_loss.append(loss)
+            if verbose and step % verbose == 0:
+                sys.stdout.write('\r{} / {} : loss = {}'.format(
+                  step, total_steps, np.mean(total_loss)))
+                sys.stdout.flush()
+
+
+        if verbose:
+            sys.stdout.write('\r')
+
+        return np.mean(total_loss), accuracy/float(total_steps)
+
+
+    def __init__(self, config):
+
+        self.config = config
+        self.variables_to_save = {}
+        self.load_data(debug=False)
+        self.add_placeholders()
+        self.add_reused_variables()
+        self.output = self.inference()
+        self.pred = self.get_predictions(self.output)
+        self.calculate_loss = self.add_loss_op(self.output)
+        self.train_step = self.add_training_op(self.calculate_loss)
+        self.merged = tf.merge_all_summaries()
+
---
+++ snli_input.py
@@ -0,0 +1,270 @@
+import sys
+import os as os
+import numpy as np
+import json
+# can be sentence or word
+input_mask_mode = "word"
+
+# adapted from https://github.com/YerevaNN/Dynamic-memory-networks-in-Theano/
+def init_snli(fname):
+    
+    print "==> Loading test from %s" % fname
+    data_all = []
+    k = 0
+    with open(fname, 'rb') as f:
+        for line in f:
+            k+= 1
+            line = line.decode('utf-8')
+            line = line.lower()
+            data = json.loads(line)
+            if data['gold_label'] == '-':
+            # ignore items without a gold label
+                continue
+
+            sentence1_parse = data['sentence1_parse']
+            sentence2_parse = data['sentence2_parse']
+            label = data['gold_label']
+
+            t = {'sent1': sentence1_parse, 'sent2':sentence2_parse , 'label':label}
+            data_all.append(t)
+
+    print("Loaded {} data".format(len(data_all)))
+    
+    return data_all
+
+
+def get_snli_raw():
+    snli_train_raw = init_snli('data/snli_1.0/snli_1.0_train.jsonl')
+    snli_dev_raw = init_snli('data/snli_1.0/snli_1.0_dev.jsonl')
+    snli_test_raw = init_snli('data/snli_1.0/snli_1.0_test.jsonl')
+    return snli_train_raw, snli_dev_raw, snli_test_raw
+
+            
+def load_glove(dim):
+    word2vec = {}
+    
+    print "==> loading glove"
+    with open(("./data/glove/glove.6B/glove.6B." + str(dim) + "d.txt")) as f:
+        for line in f:    
+            l = line.split()
+            word2vec[l[0]] = map(float, l[1:])
+            
+    print "==> glove is loaded"
+    
+    return word2vec
+
+
+def create_vector(word, word2vec, word_vector_size, silent=True):
+    # if the word is missing from Glove, create some fake vector and store in glove!
+    vector = np.random.uniform(0.0,1.0,(word_vector_size,))
+    word2vec[word] = vector
+    if (not silent):
+        print "utils.py::create_vector => %s is missing" % word
+    return vector
+
+def process_word(word, word2vec, vocab, ivocab, word_vector_size, to_return="word2vec", silent=True):
+    if not word in word2vec:
+        create_vector(word, word2vec, word_vector_size, silent)
+    if not word in vocab: 
+        next_index = len(vocab)
+        vocab[word] = next_index
+        ivocab[next_index] = word
+    
+    if to_return == "word2vec":
+        return word2vec[word]
+    elif to_return == "index":
+        return vocab[word]
+    elif to_return == "onehot":
+        raise Exception("to_return = 'onehot' is not implemented yet")
+
+
+def process_input(data_raw, floatX, word2vec, vocab, ivocab, embed_size, split_sentences=False):
+    questions = []
+    inputs = []
+    answers = []
+    input_masks = []
+
+    label_map = {'entailment':1, 'neutral':0, 'contradiction':2}
+    for x in data_raw:
+        if split_sentences:
+            inp = x["sent1"].lower().split(' . ') 
+            inp = [w for w in inp if len(w) > 0]
+            inp = [i.split() for i in inp]
+        else:
+            inp = x["sent1"].lower().split(' ') 
+            inp = [w for w in inp if len(w) > 0]
+
+        q = x["sent2"].lower().split(' ')
+        q = [w for w in q if len(w) > 0]
+
+        if split_sentences: 
+            inp_vector = [[process_word(word = w, 
+                                        word2vec = word2vec, 
+                                        vocab = vocab, 
+                                        ivocab = ivocab, 
+                                        word_vector_size = embed_size, 
+                                        to_return = "index") for w in s] for s in inp]
+        else:
+            inp_vector = [process_word(word = w, 
+                                        word2vec = word2vec, 
+                                        vocab = vocab, 
+                                        ivocab = ivocab, 
+                                        word_vector_size = embed_size, 
+                                        to_return = "index") for w in inp]
+                                    
+        q_vector = [process_word(word = w, 
+                                    word2vec = word2vec, 
+                                    vocab = vocab, 
+                                    ivocab = ivocab, 
+                                    word_vector_size = embed_size, 
+                                    to_return = "index") for w in q]
+        
+        if split_sentences:
+            inputs.append(inp_vector)
+        else:
+            inputs.append(np.vstack(inp_vector).astype(floatX))
+
+        questions.append(np.vstack(q_vector).astype(floatX))
+        
+        answers.append(label_map[x['label']])
+
+        if not split_sentences:
+            if input_mask_mode == 'word':
+                input_masks.append(np.array([index for index, w in enumerate(inp)], dtype=np.int32)) 
+            elif input_mask_mode == 'sentence': 
+                input_masks.append(np.array([index for index, w in enumerate(inp) if w == '.'], dtype=np.int32)) 
+            else:
+                raise Exception("invalid input_mask_mode")
+    
+    return inputs, questions, answers, input_masks
+
+def get_lens(inputs, split_sentences=False):
+    lens = np.zeros((len(inputs)), dtype=int)
+    for i, t in enumerate(inputs):
+        lens[i] = t.shape[0]
+    return lens
+
+def get_sentence_lens(inputs):
+    lens = np.zeros((len(inputs)), dtype=int)
+    sen_lens = []
+    max_sen_lens = []
+    for i, t in enumerate(inputs):
+        sentence_lens = np.zeros((len(t)), dtype=int)
+        for j, s in enumerate(t):
+            sentence_lens[j] = len(s)
+        lens[i] = len(t)
+        sen_lens.append(sentence_lens)
+        max_sen_lens.append(np.max(sentence_lens))
+    return lens, sen_lens, max(max_sen_lens)
+    
+
+def pad_inputs(inputs, lens, max_len, mode="", sen_lens=None, max_sen_len=None):
+    if mode == "mask":
+        padded = [np.pad(inp, (0, max_len - lens[i]), 'constant', constant_values=0) for i, inp in enumerate(inputs)]
+        return np.vstack(padded)
+
+    elif mode == "split_sentences":
+        padded = np.zeros((len(inputs), max_len, max_sen_len))
+        for i, inp in enumerate(inputs):
+            padded_sentences = [np.pad(s, (0, max_sen_len - sen_lens[i][j]), 'constant', constant_values=0) for j, s in enumerate(inp)]
+            # trim array according to max allowed inputs
+            if len(padded_sentences) > max_len:
+                padded_sentences = padded_sentences[(len(padded_sentences)-max_len):]
+                lens[i] = max_len
+            padded_sentences = np.vstack(padded_sentences)
+            padded_sentences = np.pad(padded_sentences, ((0, max_len - lens[i]),(0,0)), 'constant', constant_values=0)
+            padded[i] = padded_sentences
+        return padded
+
+    padded = [np.pad(np.squeeze(inp, axis=1), (0, max_len - lens[i]), 'constant', constant_values=0) for i, inp in enumerate(inputs)]
+    return np.vstack(padded)
+
+def create_embedding(word2vec, ivocab, embed_size):
+    embedding = np.zeros((len(ivocab), embed_size))
+    for i in range(len(ivocab)):
+        word = ivocab[i]
+        embedding[i] = word2vec[word]
+    return embedding
+
+def load_snli(config, split_sentences=False):
+    vocab = {}
+    ivocab = {}
+
+    snli_train_raw, snli_val_raw, snli_test_raw = get_snli_raw()
+
+    if config.word2vec_init:
+        assert config.embed_size == 100
+        word2vec = load_glove(config.embed_size)
+    else:
+        word2vec = {}
+
+    # set word at index zero to be end of sentence token so padding with zeros is consistent
+    process_word(word = "<eos>", 
+                word2vec = word2vec, 
+                vocab = vocab, 
+                ivocab = ivocab, 
+                word_vector_size = config.embed_size, 
+                to_return = "index")
+
+    print '==> get train inputs'
+    train_data = process_input(snli_train_raw, config.floatX, word2vec, vocab, ivocab, config.embed_size, split_sentences)
+    print '==> get validation inputs'
+    val_data = process_input(snli_val_raw, config.floatX, word2vec, vocab, ivocab, config.embed_size, split_sentences)   
+    print '==> get test inputs'
+    test_data = process_input(snli_test_raw, config.floatX, word2vec, vocab, ivocab, config.embed_size, split_sentences)
+
+    if config.word2vec_init:
+        assert config.embed_size == 100
+        word_embedding = create_embedding(word2vec, ivocab, config.embed_size)
+    else:
+        word_embedding = np.random.uniform(-config.embedding_init, config.embedding_init, (len(ivocab), config.embed_size))
+
+    inputs, questions, answers, input_masks = train_data if config.train_mode else test_data
+
+    if split_sentences:
+        input_lens, sen_lens, max_sen_len = get_sentence_lens(inputs)
+        max_mask_len = max_sen_len
+    else:
+        input_lens = get_lens(inputs)
+        mask_lens = get_lens(input_masks)
+        max_mask_len = np.max(mask_lens)
+
+    q_lens = get_lens(questions)
+
+    max_q_len = np.max(q_lens)
+    max_input_len = min(np.max(input_lens), config.max_allowed_inputs)
+    
+    #pad out arrays to max
+    if split_sentences:
+        inputs = pad_inputs(inputs, input_lens, max_input_len, "split_sentences", sen_lens, max_sen_len)
+        input_masks = np.zeros(len(inputs))
+    else:
+        # here
+        inputs = pad_inputs(inputs, input_lens, max_input_len)
+        input_masks = pad_inputs(input_masks, mask_lens, max_mask_len, "mask")
+
+    questions = pad_inputs(questions, q_lens, max_q_len)
+
+    answers = np.stack(answers)
+
+    if config.train_mode:
+        train = questions, inputs, q_lens, input_lens, input_masks, answers
+        val_q_len = get_lens(val_data[1])
+        val_input_len = get_lens(val_data[0])
+        val_mask_len = get_lens(val_data[3])
+        valid = val_data[1], val_data[0], val_q_len, val_input_len , val_data[3], val_data[2]
+
+        max_input_len = min( max(np.max(input_lens),np.max(val_input_len)), config.max_allowed_inputs)
+        max_q_len = max(max_q_len, np.max(val_q_len))
+        max_mask_len = max(max_mask_len, np.max(val_mask_len))
+        # max_input_len is the same with max_mask_len here
+
+        return train, valid, word_embedding, max_q_len, max_input_len, len(vocab)
+
+    else:
+        test = questions, inputs, q_lens, input_lens, input_masks, answers
+
+        return test, word_embedding, max_q_len, max_input_len, len(vocab)
+
+
+    
